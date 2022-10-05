42a6e8171eaebc25c1883ce1916776fe452d856a
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-10-05 18:07:29 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML ="<p>Hello, World!</p>"
+}
